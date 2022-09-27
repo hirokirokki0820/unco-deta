89a013633a4d87e47d468a26b99b16ddb8310db1
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-09-27 17:34:00 +0900

    Add: Deviseコントローラーの導入($ rails g devise:controllers users), Turboへの対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "home#top"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
 end
