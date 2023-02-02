5be2f5b5780f1cb3f84bc56478f950904f0100da
hirokirokki0820 <hirokirokki0820@gmail.com>
2023-02-02 21:52:35 +0900

    ボタン作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",
-    passwords: "users/passwords"
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
